5d4699f84441fa1e7dc744b93ccd10624a809e6a
Saulius Petraitis <saulius.petraitis@vssa.lt>
2025-12-08 14:29:40 +0200

    Pakeitimas i MD

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,10 @@ release = "'0.1'"
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    'sphinx_rtd_theme',
+    'myst_parser',  # Pridėkite šią eilutę!
+]
 
 templates_path = ['_templates']
 exclude_patterns = []
---
+++ docs/source/index.md
@@ -0,0 +1,1 @@
+# Valstybės duomenų dokumentacija 
