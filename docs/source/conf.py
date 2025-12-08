20c3843958e7b331b432a02006f35eb749210699
Saulius Petraitis <saulius.petraitis@vssa.lt>
2025-12-08 14:16:48 +0200

    Initial structure

---
+++ docs/source/conf.py
@@ -0,0 +1,28 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Valstybės duomenų dokumentacija'
+copyright = '2025, Valstybės skaitmeninių sprendimų agentūra'
+author = 'Valstybės skaitmeninių sprendimų agentūra'
+release = "'0.1'"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+language = 'lt'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
